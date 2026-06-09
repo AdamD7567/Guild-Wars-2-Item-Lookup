236c993084e73683112aef568e2d136a30906fc6
Adam Imane <adamdeardon@hotmail.com>
2026-06-09 14:05:56 +0100

    - Validation for invalid input added. - TODOs Added.

--- Guild Wars 2 Projects/basic_item_lookup.py
+++ Guild Wars 2 Projects/basic_item_lookup.py
@@ -1,18 +1,24 @@
 import requests
 
+# TODO: Get price information (Trading post).
+# TODO: Allow for names to be entered to get information.
+# TODO: Allow for repeated attempts after an invalid input.
+
 # Input
 item = input("Enter the ID of the item you are looking for: ")
 
-# Using requests.get to get the input item's information using ID
-response = requests.get(f"https://api.guildwars2.com/v2/items/{item}")
-data = response.json()
-
-# Making sure we get a safe return for debugging purposes.
-if response.status_code == 200:
-    data = response.json()
-    print(data["name"])
+# Validate
+if not item.isdigit():
+    print("Please enter a numeric item ID.")
 
 else:
-    print("Something went wrong.")
-    print("Status code:", response.status_code)
-    print(response.text)
+    # Using requests.get to get the input item's information using ID
+    response = requests.get(f"https://api.guildwars2.com/v2/items/{item}")
+    data = response.json()
+
+    if response.status_code == 200:
+        print(data["name"])
+    
+    else:
+        print("Could not find that item.")
+        print("API message:", data.get("text", "Unknown error"))
